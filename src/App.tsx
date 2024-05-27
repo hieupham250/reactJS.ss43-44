1dcc9d44753d2ef0bda666eff71e5e2de5b29bc5
hieupham250 <ptrunghieu2507@gmail.com>
2024-05-27 09:35:50 +0700

    add

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/Login";
+
+export default function App() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+}
